39b13398736c8ecaf1080054847efea63c7cc349
Ahmad Siddique <asshikrani66@gmail.com>
2026-07-30 07:44:45 +0000

    image issue has been solved

--- app/web/src/app/blog/[slug]/page.tsx
+++ app/web/src/app/blog/[slug]/page.tsx
@@ -19,9 +19,18 @@ const page = async ({ params }: Props) => {
   const blog = await getBlog(slug);
   return (
     <div>
-      <h1>{blog[0].title}</h1>
-      <p>{blog[0].description}</p>
-      <Image src={blog[0].image || ""} alt="not to show" height={400} width={400}/> { /* TODO: put here some temp img*/}
+      <h1 className="scroll-m-20 my-6 text-4xl font-bold tracking-tight lg:text-5xl">{blog[0].title}</h1>
+      <blockquote className="my-6 border-l-4 border-zinc-700 pl-6 italic text-zinc-400">
+        {blog[0].description}
+      </blockquote>
+      <Image 
+      src={blog[0].image || ""}
+        alt={blog[0].title ?? "sample image"}
+        width={900}
+        height={500}
+      sizes="(max-width: 768px) 100vw, 900px"
+        className="my-8 rounded-lg border border-zinc-800"
+      />
       <PortableText 
       value={blog[0].detail}
       components={portableTextComponents}
