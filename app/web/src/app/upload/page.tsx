dd4e05e5b66d9b61e84edafdea5e78bd2a32d9b1
Ahmad Siddique <asshikrani66@gmail.com>
2026-08-07 09:00:25 +0500

    a little message

--- app/web/src/app/upload/page.tsx
+++ app/web/src/app/upload/page.tsx
@@ -5,8 +5,11 @@ const page = () => {
   return (
     <main className="mx-auto px-7 max-w-2xl pt-12 max-h-screen overflow-y-hidden py-6">
         <h1 className="text-3xl font-bold shadow-lg tracking-tight ">File Uploader</h1>
-        <p className="text-sm text-muted-foreground py-7">
+        <p className="text-sm text-muted-foreground py-4">
             Upload your files here.
+        </p>
+        <p className="text-xs pb-7 text-muted-foreground">
+            (Please! don't upload large file I'm not running separate worker it's all on single thread)
         </p>
         <FileUploadDirectUpload />
     </main>
