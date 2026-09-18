73a390ea4a22ce6450c1d2f9fa79193665ba18f0
Ahmad Siddique <asshikrani66@gmail.com>
2026-09-18 15:32:46 +0500

    feat: update getBlogs and getProjects functions to include cache options

--- web/src/app/blog/_lib/get-blogs.ts
+++ web/src/app/blog/_lib/get-blogs.ts
@@ -3,5 +3,7 @@ import { getBlogsQuery } from "@/sanity/lib/queries";
 import { GetBlogsQueryResult } from "@/sanity/types";
 
 export async function getBlogs() {
-  return await client.fetch<GetBlogsQueryResult>(getBlogsQuery);
+  return await client.fetch<GetBlogsQueryResult>(getBlogsQuery, {}, {
+    cache: "no-store"
+  });
 }
--- web/src/app/project/_lib/get-projects.ts
+++ web/src/app/project/_lib/get-projects.ts
@@ -3,6 +3,7 @@ import { getProjectsQuery } from '@/sanity/lib/queries'
 import { GetProjectsQueryResult } from '@/sanity/types';
 
 export async function getProjects() {
-  return await client.fetch<GetProjectsQueryResult>(getProjectsQuery);
-
+  return await client.fetch<GetProjectsQueryResult>(getProjectsQuery, {}, {
+    cache: "no-cache" 
+  });
 }
