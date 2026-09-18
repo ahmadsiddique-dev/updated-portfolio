53244ce4c805e09027be6c55f552e4d074554ec9
Ahmad Siddique <asshikrani66@gmail.com>
2026-09-18 15:21:15 +0500

    refactor: simplify getBlogs and getProjects functions by removing sanityFetch

--- web/src/app/blog/_lib/get-blogs.ts
+++ web/src/app/blog/_lib/get-blogs.ts
@@ -1,15 +1,7 @@
-// import { client } from "@/sanity/client";
+import { client } from "@/sanity/client";
 import { getBlogsQuery } from "@/sanity/lib/queries";
 import { GetBlogsQueryResult } from "@/sanity/types";
-import { sanityFetch } from '@/sanity/live'
 
 export async function getBlogs() {
-  // return await client.fetch<GetBlogsQueryResult>(getBlogsQuery);
-  const { data } = await sanityFetch({
-    query: getBlogsQuery,
-    perspective: "published",
-    stega: false
-  })
-
-  return data as GetBlogsQueryResult;
+  return await client.fetch<GetBlogsQueryResult>(getBlogsQuery);
 }
--- web/src/app/project/_lib/get-projects.ts
+++ web/src/app/project/_lib/get-projects.ts
@@ -1,15 +1,8 @@
 import { client } from '@/sanity/client';
 import { getProjectsQuery } from '@/sanity/lib/queries'
 import { GetProjectsQueryResult } from '@/sanity/types';
-import { sanityFetch } from '@/sanity/live'
 
 export async function getProjects() {
-  // return await client.fetch<GetProjectsQueryResult>(getProjectsQuery);
-  const { data } = await sanityFetch({
-    query: getProjectsQuery,
-    perspective: "published",
-    stega: false
-  })
+  return await client.fetch<GetProjectsQueryResult>(getProjectsQuery);
 
-  return data as GetProjectsQueryResult;
 }
